6caef2e14102946f41ed7c4504331167acf7d7ce
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-01-23 11:53:06 -0300

    feat: validations

--- zuum/src/main/java/com/example/zuum/Ride/Dto/NewRideDTO.java
+++ zuum/src/main/java/com/example/zuum/Ride/Dto/NewRideDTO.java
@@ -10,11 +10,27 @@ import com.example.zuum.Ride.RideModel;
 import com.example.zuum.User.UserModel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record NewRideDTO(
-        @JsonProperty("user_id") Integer userId,
-        BigDecimal price,
-        Point origin,
-        Point destiny) {
+    @JsonProperty("user_id")
+    @NotBlank(message = "User id cannot be blank")
+    @NotNull(message = "User id cannot be null")
+    Integer userId,
+    
+    @NotBlank(message = "Price cannot be blank")
+    @NotNull(message = "Price cannot be null")
+    BigDecimal price,
+    
+    @NotBlank(message = "Origin cannot be blank")
+    @NotNull(message = "Origin cannot be null")
+    Point origin,
+    
+    @NotBlank(message = "Destiny cannot be blank")
+    @NotNull(message = "Destiny cannot be null")
+    Point destiny
+) {
 
     public RideModel toRideModel(UserModel user) {
         GeometryFactory geometryFactory = new GeometryFactory();
