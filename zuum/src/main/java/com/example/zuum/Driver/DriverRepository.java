5a8fde9e2657f3a29cf299a8bc14bebc8f3da313
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-01-23 10:57:16 -0300

    feat: new queries

--- zuum/src/main/java/com/example/zuum/Driver/DriverRepository.java
+++ zuum/src/main/java/com/example/zuum/Driver/DriverRepository.java
@@ -1,8 +1,21 @@
 package com.example.zuum.Driver;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverRepository extends JpaRepository<DriverModel, Integer> {
+
+    @Query(
+        value = "SELECT * FROM driver WHERE ST_Distance(driver.curr_location, ST_MakePoint(:lat, :longt)) <= 1000",
+        nativeQuery = true
+    )
+    List<DriverModel> getDriverByDistance(double lat, double longt);
+
+    Optional<DriverModel> findByUserId(Integer userId);
+
 }
