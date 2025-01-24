cd4fe210f48548bd1617d4818382a1536ddbce6d
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-01-24 13:58:30 -0300

    refactor: deleted unnecessary method

--- zuum/src/main/java/com/example/zuum/Driver/DriverWebSocketController.java
+++ zuum/src/main/java/com/example/zuum/Driver/DriverWebSocketController.java
@@ -5,26 +5,16 @@ import java.security.Principal;
 import org.slf4j.Logger;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.example.zuum.Common.utils;
 import com.example.zuum.Driver.Dto.LocationDTO;
-import com.example.zuum.Trip.Dto.TripRequestNotificationDTO;
 
 @Controller
 @MessageMapping("/drivers")
 public record DriverWebSocketController(DriverService service, DriverNotifier notifier) {
     
     static Logger LOGGER = utils.getLogger(DriverWebSocketController.class);
-
-    @MessageMapping("/request")
-    @SendTo("/topic/trip-request")
-    public TripRequestNotificationDTO handleNewTripRequest(TripRequestNotificationDTO notificationDTO) {
-        LOGGER.info("Notifying new trip request");
-
-        return notificationDTO;
-    }
 
     @MessageMapping("/location")
     public void updateCurrentLocation(LocationDTO dto, Principal principal) {
