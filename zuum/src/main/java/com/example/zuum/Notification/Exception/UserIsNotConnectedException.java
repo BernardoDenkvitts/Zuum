07531b9e7cd046ef2c500113da3c26d1ebe8040c
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-01-25 02:27:29 -0300

    refactor: created package for Notification

---
+++ zuum/src/main/java/com/example/zuum/Notification/Exception/UserIsNotConnectedException.java
@@ -0,0 +1,7 @@
+package com.example.zuum.Notification.Exception;
+
+public class UserIsNotConnectedException extends RuntimeException {
+    public UserIsNotConnectedException(String message) {
+        super(message);
+    }
+}
---
+++ zuum/src/main/java/com/example/zuum/Notification/WsNotifier.java
@@ -0,0 +1,71 @@
+package com.example.zuum.Notification;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpSubscription;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.stereotype.Component;
+
+import com.example.zuum.Common.utils;
+import com.example.zuum.Driver.DriverModel;
+import com.example.zuum.Driver.DriverService;
+import com.example.zuum.Notification.Exception.UserIsNotConnectedException;
+import com.example.zuum.Ride.Dto.RideRequestNotificationDTO;
+
+@Component
+public record WsNotifier(
+        SimpUserRegistry registry,
+        SimpMessagingTemplate template,
+        DriverService service) {
+
+    static Logger LOGGER = utils.getLogger(WsNotifier.class);
+
+    public void notifyUser(String userIdentifier, String destination, Object payload) {
+        if (registry.getUser(userIdentifier) == null) {
+            LOGGER.info("User {} is not connected", userIdentifier);
+            throw new UserIsNotConnectedException("The user to be notified is not connected");
+        }
+        LOGGER.info("Sending payload to User {}", userIdentifier);
+        template.convertAndSendToUser(userIdentifier, destination, payload);
+    }
+
+    public void newRideRequest(RideRequestNotificationDTO dto, List<DriverModel> drivers) {
+        String rideRequestQueue = "/queue/ride-request";
+        Set<SimpUser> subscribedDrivers = getSubscribers("/user" + rideRequestQueue);
+        List<SimpUser> drivesToNotify = findOnlineNearbyDrivers(subscribedDrivers, drivers);
+
+        notifyDrivers(drivesToNotify, rideRequestQueue, dto);
+    }
+
+    // Get drivers subscribed in destination (queue/topic)
+    private Set<SimpUser> getSubscribers(String destination) {
+        Set<SimpUser> subscribers = new HashSet<>();
+        Set<SimpSubscription> subscriptions = registry
+                .findSubscriptions(sub -> sub.getDestination().equals(destination));
+        for (SimpSubscription sub : subscriptions) {
+            subscribers.add(sub.getSession().getUser());
+        }
+
+        return subscribers;
+    }
+
+    private List<SimpUser> findOnlineNearbyDrivers(Set<SimpUser> subscribedDrivers, List<DriverModel> nearbyDrivers) {
+        return subscribedDrivers.stream()
+                .filter(sub -> nearbyDrivers.stream()
+                        .anyMatch(driver -> String.valueOf(driver.getUser().getId()).equals(sub.getName())))
+                .toList();
+    }
+
+    private void notifyDrivers(List<SimpUser> driversToNotify, String queue, RideRequestNotificationDTO dto) {
+        for (SimpUser driver : driversToNotify) {
+            LOGGER.info("Sending new ride request to driver {}", driver.getName());
+            template.convertAndSendToUser(driver.getName(), queue, dto);
+        }
+    }
+
+}
