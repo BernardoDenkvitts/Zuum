d0611a6ef419e56962c8db14890017044e3275f5
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-01-27 12:27:51 -0300

    refactor: attributes

--- zuum/src/main/java/com/example/zuum/Driver/Dto/DriverResponseDTO.java
+++ zuum/src/main/java/com/example/zuum/Driver/Dto/DriverResponseDTO.java
@@ -1,5 +1,7 @@
 package com.example.zuum.Driver.Dto;
 
+import org.locationtech.jts.geom.Point;
+
 import com.example.zuum.Driver.DriverModel;
 import com.example.zuum.User.Dto.UserResponseDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -15,10 +17,13 @@ public record DriverResponseDTO(
     String driverLicense,
 
     @JsonProperty("user")
-    UserResponseDTO user
+    UserResponseDTO user,
+
+    @JsonProperty("curr_location")
+    Point currLocation
 ) {
     public static DriverResponseDTO create(DriverModel model) {
         return new DriverResponseDTO(model.getId(), model.getPlate(),
-                 model.getCarModel(), model.getDriverLicense(), UserResponseDTO.create(model.getUser()));
+                 model.getCarModel(), model.getDriverLicense(), UserResponseDTO.create(model.getUser()), model.getCurrLocation());
     }
 } 
