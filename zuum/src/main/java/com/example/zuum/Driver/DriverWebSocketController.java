97b3436ee7fdd2a88e6e051e1e7ff7bd17381035
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-02-01 18:41:42 -0300

    refactor: new queue name, added WsMessageDTO

--- zuum/src/main/java/com/example/zuum/Driver/DriverWebSocketController.java
+++ zuum/src/main/java/com/example/zuum/Driver/DriverWebSocketController.java
@@ -10,20 +10,24 @@ import org.springframework.stereotype.Controller;
 import com.example.zuum.Common.utils;
 import com.example.zuum.Driver.Dto.LocationDTO;
 import com.example.zuum.Notification.WsNotifier;
+import com.example.zuum.Notification.Dto.WsMessageDTO;
+import com.example.zuum.Notification.Dto.WsMessageType;
 
 @Controller
 public record DriverWebSocketController(DriverService service, WsNotifier notifier) {
 
     static Logger LOGGER = utils.getLogger(DriverWebSocketController.class);
 
+    private static final String queue = "/queue/driver/reply"; 
+
     @MessageMapping("/drivers/location")
     public void updateCurrentLocation(LocationDTO dto, Principal principal) {
         try {
             LOGGER.info("Updating driver {} location", dto.driverId());
             service.updateLocation(dto);
-            notifier.notifyUser(principal.getName(), "/queue/reply", "Success: location updated");
+            notifier.notifyUser(principal.getName(), queue, new WsMessageDTO(WsMessageType.DRIVER_LOCATION_UPDATE, "Updated"));
         } catch(Exception e) {
-            notifier.notifyUser(principal.getName(), "/queue/reply", "Error: " + e.getMessage());
+            notifier.notifyUser(principal.getName(), queue, new WsMessageDTO(WsMessageType.ERROR, "Error: " + e.getMessage()));
         }
     }
 
